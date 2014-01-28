a2122d5a709bc2403d26c73e528bc4799f1e1eab
Todd Wolfson <todd@twolfson.com>
2014-01-27 22:17:59 -0800

    Starting to fill out multipart test

--- test/eight-track-normalize-multipart_test.js
+++ test/eight-track-normalize-multipart_test.js
@@ -1,8 +1,21 @@
+var fs = require('fs');
+var expect = require('chai').expect;
+var express = require('express');
 var normalizeMultipart = require('../');
+var httpUtils = require('./utils/http');
+var serverUtils = require('./utils/server');
 
 describe('An `eight-track` server using `normalize-multipart`', function () {
-  before(function () {
-
+  serverUtils.run(1337, function (req, res) {
+    express.multipart()(req, res, function (err) {
+      if (err) { throw err; }
+      res.send(req.body);
+    });
+  });
+  serverUtils.runEightServer(1338, {
+    url: 'http://localhost:1337',
+    fixtureDir: __dirname + '/actual-files/normalize',
+    normalizeFn: normalizeMultipart
   });
 
   describe('receiving a multipart form request', function () {
